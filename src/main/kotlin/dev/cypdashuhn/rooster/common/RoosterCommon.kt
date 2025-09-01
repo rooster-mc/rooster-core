325495f74c1e5f64c4c32d0b364128f267aa8a6c
CypDasHuhn <Martinfischer533@gmail.com>
2025-09-01 22:14:25 +0200

    d

--- src/main/kotlin/dev/cypdashuhn/rooster/common/RoosterCommon.kt
+++ src/main/kotlin/dev/cypdashuhn/rooster/common/RoosterCommon.kt
@@ -5,9 +5,16 @@ import java.util.logging.Logger
 
 object RoosterCommon {
     var logger: Logger = Logger.getLogger("RoosterCommon")
-    lateinit var plugin: JavaPlugin
+    private lateinit var pluginInst: JavaPlugin
+    val plugin: JavaPlugin
+        get() {
+            if (!::pluginInst.isInitialized) {
+                throw UninitializedPropertyAccessException("You need to call RoosterCommon.init() before using RoosterCommon")
+            }
+            return pluginInst
+        }
 
     fun init(plugin: JavaPlugin) {
-        this.plugin = plugin
+        this.pluginInst = plugin
     }
 }

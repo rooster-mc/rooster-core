22a6f6f307313f478909452ea2b7a01e1755adb1
cyp <martinfischer533@gmail.com>
2026-06-05 01:44:25 +0200

    kyori utils

--- src/main/kotlin/dev/rooster/core/util/TextUtils.kt
+++ src/main/kotlin/dev/rooster/core/util/TextUtils.kt
@@ -1,5 +1,9 @@
 package dev.rooster.core.util
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
 
 fun String.toComponent() = Component.text(this)
+
+fun minimessage(text: String): Component = MiniMessage.miniMessage().deserialize(text)
+fun mm(text: String) = minimessage(text)
